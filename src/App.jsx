d8e47397ca3a9588ef22974aa9e04195c852128a
camosquerp <68928380+camosquerp@users.noreply.github.com>
2022-09-23 09:49:29 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+//import Home from "./pages/Home"; 
+//import Carrito from "./pages/Carrito";
+//import IniciarSesion from "./pages/IniciarSesion";
+import Registro from "./pages/Registro";
+
+const App = () => {
+  return <Registro/>;
+};
+
+export default App;
---
+++ src/Data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/XsdmR2c/1.png",
+        title: "!BIENVENIDOS¡",
+        desc: "EL MEJOR PROVEEDOR DE PRODUCTOS PARA EL RIEGO DE TUS CULTIVOS.",
+        bg: "f5fafd",
+      },
+      {
+        id: 2,
+        img: "https://i.ibb.co/tmKhLCN/Slider2.png",
+        title: "RIEGO AUTOMATICO",
+        desc: "LA MEJOR INVERSION PARA TU CULTIVO.",
+        bg: "fcf1ed",
+      },
+      {
+        id: 3,
+        img: "https://i.ibb.co/1RcVGHJ/Slider3.png",
+        title: "AGRORIEGOS",
+        desc: "LOS MEJORES PRODUCTOS E INSUMOS PARA TU CULTIVO.",
+        bg: "fbf0f4",
+      },
+    ];
